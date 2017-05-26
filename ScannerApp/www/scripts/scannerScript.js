264789e9633e70e33c44f42931c7afcb0cc8a19c
John Redden <jtredden@gmail.com>
2017-05-25 19:37:51 -0700

    Initial Upload
    test

---
+++ ScannerApp/www/scripts/scannerScript.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    console.log("Document ready!");
+
+    $("#myScanButton").click(function () {
+
+        console.log("Button clicked!");
+        //$("#myScanOutput").html("Hello jQuery!");
+
+        scan();
+
+    });
+
+    function scan() {
+        //cordova takes care of business!
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                if (!result.cancelled) {
+                    //only want QR code scanner functionality
+                    if (result.format == "QR_CODE") {
+                        //just add QR text to page
+                        $("#myScanOutput").html(result.text);
+
+                    }
+                }
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    }
+
+
+});
