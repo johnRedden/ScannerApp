0631650913945f079e64757156b70724d7ee1149
John Redden <jtredden@gmail.com>
2017-06-10 14:54:09 -0700

    Added Array of Locations and dynamically populed the list
    Used an array of location to populate the list items in the scan page.
    It works, try it.

--- ScannerApp/www/scripts/customscripts/scannerScript.js
+++ ScannerApp/www/scripts/customscripts/scannerScript.js
@@ -1,10 +1,15 @@
 ﻿$(document).ready(function () {
 
+    //populate this array from database!
+    var locations = ["library", "reg-window", "bookstore", "cafeteria", "kaweah", "john muir","nursing office","administration","the quad"];
+    //dynamically populate location listview from this array or database.
+    populateLocationList(locations);
+    
+
     $("#scanQRcodeBtn").click(function () {
         //console.log("QR button clicked")
         scan();
     });
-
 
     function scan() {
         //cordova takes care of business!
@@ -15,6 +20,12 @@
                     if (result.format == "QR_CODE") {
                         //just add QR text to page
                         $("#scanOutput").html(result.text);
+                        var indexOfScannedLocation = locations.indexOf(result.text);
+
+                        console.log(locations[indexOfScannedLocation]);
+                        //turn list item green to indicate that it was visited
+                        $("#" + locations[indexOfScannedLocation]+">a").addClass("visited");
+                        //Todo:  Add points for found location.
                     }
                 }
             },
@@ -23,6 +34,17 @@
             }
         );
     }
+
+    function populateLocationList(locations) {
+        $("#locationList").html("");
+
+        for (i = 0; i < locations.length; i++) {
+            var newListElem = "<li id='" + locations[i] + "' data-icon='location'><a href='#'>" + locations[i] +"</a></li>";
+            $("#locationList").append(newListElem);
+
+        }
+    
+    };
 
 
 });
