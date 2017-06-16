4d61e530171cc94beb9cd8953e2fa83d17269729
John Redden <jtredden@gmail.com>
2017-06-15 20:42:05 -0700

    Firebase is Working
    I will demo this FNL

--- ScannerApp/www/scripts/customscripts/login.js
+++ ScannerApp/www/scripts/customscripts/login.js
@@ -2,7 +2,41 @@
 score = 0;
 name = null;
 
-$(document).read(function () {
+$(document).ready(function () {
+    console.log("Im in");
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCgTypuim9iUb3n0UteN7OriatTjn2vG9Q",
+        authDomain: "mytest1-2d933.firebaseapp.com",
+        databaseURL: "https://mytest1-2d933.firebaseio.com",
+        projectId: "mytest1-2d933",
+        storageBucket: "",
+        messagingSenderId: "1007415208435"
+    };
+    firebase.initializeApp(config);
+
+    // Get a reference to the database service
+    var database = firebase.database();
+    /*
+    database.ref('users/').set({
+        username: "john",
+        email: "ee"
+    });
+    */
+    
+
+    $("#fireTest").on('click',function () {
+
+        var userRef = firebase.database().ref('users/');
+        userRef.on('value', function (snapshot) {
+            console.log(snapshot.val().username);
+            $("#fireTest").html(snapshot.val().username)
+        });
+
+
+
+    });
 
 
 
