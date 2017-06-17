ce5a055ade9e2ab50529edf637760f5063b4f51c
John Redden <jtredden@gmail.com>
2017-06-17 15:36:00 -0700

    Added New Database Connection
    And hooked mainMessage on the front page.  Real-time messaging!

--- ScannerApp/www/scripts/customscripts/login.js
+++ ScannerApp/www/scripts/customscripts/login.js
@@ -3,40 +3,30 @@ score = 0;
 name = null;
 
 $(document).ready(function () {
-    console.log("Im in");
-
+    
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyCgTypuim9iUb3n0UteN7OriatTjn2vG9Q",
-        authDomain: "mytest1-2d933.firebaseapp.com",
-        databaseURL: "https://mytest1-2d933.firebaseio.com",
-        projectId: "mytest1-2d933",
+        apiKey: "AIzaSyBu0_y7pEs9dAC-1j1CB7Lq_I7HlHjfvnM",
+        authDomain: "tcoeapp.firebaseapp.com",
+        databaseURL: "https://tcoeapp.firebaseio.com",
+        projectId: "tcoeapp",
         storageBucket: "",
-        messagingSenderId: "1007415208435"
+        messagingSenderId: "405307790686"
     };
-    firebase.initializeApp(config);
-
+    firebase.initializeApp(config);
     // Get a reference to the database service
     var database = firebase.database();
-    /*
-    database.ref('users/').set({
-        username: "john",
-        email: "ee"
+
+    // Firebase mainMessage listener (realtime database!)
+    var messageRef = firebase.database().ref('mainMessage');
+    messageRef.on('value', function (snapshot) {
+        console.log(snapshot.val().message);
+        $("#appMessage").html(snapshot.val().message);
     });
-    */
+  
     
 
-    $("#fireTest").on('click',function () {
-
-        var userRef = firebase.database().ref('users/');
-        userRef.on('value', function (snapshot) {
-            console.log(snapshot.val().username);
-            $("#fireTest").html(snapshot.val().username)
-        });
-
-
-
-    });
+ 
 
 
 
